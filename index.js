566cda32b44c94c26e543fedfcb3b4873577b711
Matt Huwiler <huwilerm@champlain.edu>
2020-01-02 00:55:47 -0500

    updated port

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var express = require('express'),
+const express = require('express'),
     app = express(),
-    path = require('path');
+    path = require('path'),
+    port = process.env.PORT || 3000;;
 
 app.use(express.static('public'));
 
@@ -12,4 +13,4 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.listen(8080);
+app.listen(port);
