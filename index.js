a0016f7cb9f95f5316112420866c85f1b5fc9572
Matt Huwiler <huwilerm@champlain.edu>
2020-01-02 00:40:24 -0500

    heroku initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express'),
+    app = express(),
+    path = require('path');
+
+app.use(express.static('public'));
+
+app.get('/game', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/game.html'));
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.listen(8080);
