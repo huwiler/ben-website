c1dbaf77a0d0920135480943d63134cd3bd1ea4a
Matt Huwiler <huwilerm@champlain.edu>
2020-01-02 08:45:34 -0500

    debugging node app h10 error

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const express = require('express'),
 
 app.use(express.static('public'));
 
-app.get('/game', function(req, res) {
+app.get('/game', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/game.html'));
 });
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT, () => {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
